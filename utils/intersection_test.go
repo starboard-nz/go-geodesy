510fc8aa3983af7c7a9b1e36a67d14f7143a868a
Nick Wittenberg <nick@xerra.nz>
2024-05-07 12:38:55 +1200

    Update tests

--- utils/intersection_test.go
+++ utils/intersection_test.go
@@ -22,7 +22,7 @@ func TestSegmentIntersection(t *testing.T) {
 		is := utils.SegmentIntersection(p1, p2, q1, q2)
 		require.NotNil(t, is)
 		assert.InDeltaf(t, is[0], 10, δ, "Longitude: %f", is[0])
-		assert.InDeltaf(t, is[1], 10.15589, δ, "Latitude: %f", is[0])
+		assert.InDeltaf(t, is[1], 10.15589, δ, "Latitude: %f", is[1])
 	})
 
 	t.Run("Parallel", func(t *testing.T) {
@@ -35,7 +35,7 @@ func TestSegmentIntersection(t *testing.T) {
 		require.Nil(t, is)
 	})
 
-	t.Run("Colinear", func(t *testing.T) {
+	t.Run("Collinear", func(t *testing.T) {
 		p1 := orb.Point{0, 0}
 		p2 := orb.Point{20, 20}
 		q1 := orb.Point{10, 10}
@@ -43,16 +43,20 @@ func TestSegmentIntersection(t *testing.T) {
 
 		is := utils.SegmentIntersection(p1, p2, q1, q2)
 		require.NotNil(t, is)
+		// NOTE the value for is will be set but returning an precise value doesn't really make sense for collinear segments.
 	})
 
-	t.Run("Colinear sharing a point", func(t *testing.T) {
+	t.Run("Collinear sharing a point", func(t *testing.T) {
 		p1 := orb.Point{0, 0}
 		p2 := orb.Point{20, 20}
 		q1 := orb.Point{20, 20}
 		q2 := orb.Point{40, 40}
 
+		const δ = 0.0001
 		is := utils.SegmentIntersection(p1, p2, q1, q2)
 		require.NotNil(t, is)
+		assert.InDeltaf(t, is[0], 20, δ, "Longitude: %f", is[0])
+		assert.InDeltaf(t, is[1], 20, δ, "Latitude: %f", is[1])
 	})
 
 	t.Run("Sharing a point", func(t *testing.T) {
@@ -61,8 +65,11 @@ func TestSegmentIntersection(t *testing.T) {
 		q1 := orb.Point{20, 20}
 		q2 := orb.Point{40, 60}
 
+		const δ = 0.0001
 		is := utils.SegmentIntersection(p1, p2, q1, q2)
 		require.NotNil(t, is)
+		assert.InDeltaf(t, is[0], 20, δ, "Longitude: %f", is[0])
+		assert.InDeltaf(t, is[1], 20, δ, "Latitude: %f", is[1])
 	})
 
 	t.Run("Middle of one line intersects point of the other", func(t *testing.T) {
@@ -71,8 +78,11 @@ func TestSegmentIntersection(t *testing.T) {
 		q1 := orb.Point{20, 0}
 		q2 := orb.Point{20, 40}
 
+		const δ = 0.0001
 		is := utils.SegmentIntersection(p1, p2, q1, q2)
 		require.NotNil(t, is)
+		assert.InDeltaf(t, is[0], 20, δ, "Longitude: %f", is[0])
+		assert.InDeltaf(t, is[1], 20, δ, "Latitude: %f", is[1])
 	})
 
 	t.Run("No intersection", func(t *testing.T) {
@@ -85,34 +95,34 @@ func TestSegmentIntersection(t *testing.T) {
 		require.Nil(t, is)
 	})
 
+	// AM crosing lines don't intersect across the AM - they intersect at 0
+	// NOTE we probably want to fix this.
+	t.Run("Intersection on AM crossing line and prime meridian", func(t *testing.T) {
+		p1 := orb.Point{170, 10}
+		p2 := orb.Point{-170, -10}
+		q1 := orb.Point{0, 10}
+		q2 := orb.Point{0, -10}
+
+		const δ = 0.0001
+		is := utils.SegmentIntersection(p1, p2, q1, q2)
+		require.NotNil(t, is) // Should actually expect this to be nil when we update the code
+		assert.InDeltaf(t, is[0], 0, δ, "Longitude: %f", is[0])
+		assert.InDeltaf(t, is[1], 0, δ, "Latitude: %f", is[1])
+	})
+
+	// AM crosing lines don't intersect across the AM - they intersect at 0
+	// NOTE we probably want to fix this.
 	t.Run("Intersection on AM crossing lines", func(t *testing.T) {
 		p1 := orb.Point{170, 10}
 		p2 := orb.Point{-170, -10}
 		q1 := orb.Point{-170, 10}
 		q2 := orb.Point{170, -10}
 
+		const δ = 0.0001
 		is := utils.SegmentIntersection(p1, p2, q1, q2)
 		require.NotNil(t, is)
-	})
-
-	t.Run("Intersection on AM crossing lines", func(t *testing.T) {
-		p1 := orb.Point{170, 10}
-		p2 := orb.Point{-170, -20}
-		q1 := orb.Point{-170, 20}
-		q2 := orb.Point{170, -10}
-
-		is := utils.SegmentIntersection(p1, p2, q1, q2)
-		require.NotNil(t, is)
-	})
-
-	t.Run("Intersection on AM crossing lines", func(t *testing.T) {
-		p1 := orb.Point{170, 10}
-		p2 := orb.Point{-170, -10}
-		q1 := orb.Point{-175, 10}
-		q2 := orb.Point{-175, -10}
-
-		is := utils.SegmentIntersection(p1, p2, q1, q2)
-		require.NotNil(t, is)
+		assert.InDeltaf(t, is[0], 0, δ, "Longitude: %f", is[0])
+		assert.InDeltaf(t, is[1], 0, δ, "Latitude: %f", is[1])
 	})
 }
 
@@ -137,7 +147,7 @@ func TestSegmentsIntersect(t *testing.T) {
 		require.False(t, is)
 	})
 
-	t.Run("Colinear sharing a point", func(t *testing.T) {
+	t.Run("Collinear sharing a point", func(t *testing.T) {
 		p1 := orb.Point{0, 0}
 		p2 := orb.Point{20, 20}
 		q1 := orb.Point{20, 20}
